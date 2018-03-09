b7034855d4a6d924b0506d5cf56ea7f92defc584
Ben Wiedermann <benw@cs.hmc.edu>
2018-03-09 15:19:20 -0800

    Adding Layout component, with filler text in the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Layout>
+        <p>
+          Excepteur incididunt aliquip occaecat tempor cillum sunt officia 
+          pariatur nisi mollit. Cillum in reprehenderit eiusmod anim ea duis in 
+          Lorem nostrud est. Excepteur excepteur tempor aliqua dolore culpa 
+          Lorem sint incididunt et.
+        </p>
+      </Layout>
     );
   }
 }
