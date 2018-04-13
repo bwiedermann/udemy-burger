e49c044c85eada372399713dfa72b0f0ea4c19ba
Ben Wiedermann <benw@cs.hmc.edu>
2018-04-13 00:10:20 -0700

    closes #44

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import Burger from "../../components/Burger/Burger";
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -58,7 +59,23 @@ class BurgerBuilder extends Component {
 
     order = () => this.setState({purchasing: true})
     cancelOrder = () => this.setState({purchasing: false})
-    continueOrder = () => alert('You continue!')
+
+    continueOrder = () => {
+        //alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,  // should calculate on server
+            customer: {
+                name: 'Ben',
+                address: '123 Main St.',
+                zip: '11111',
+                email: 'test@test.com',
+            }
+        }
+        axios.post('/orders.json', order)
+             .then(response => console.log(response))
+             .catch(error => console.log(error));
+    }
 
     render() { 
         const disabledInfo = {...this.state.ingredients};
