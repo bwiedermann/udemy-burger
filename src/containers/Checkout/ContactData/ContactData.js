1cc9f95fa62e6a85832b0333380f64af2e9efe02
Ben Wiedermann <benw@cs.hmc.edu>
2018-04-13 18:47:00 -0700

    Posting results to firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Form, Card } from 'semantic-ui-react';
+import axios from '../../../axios-orders';
 
 export class ContactData extends Component {
   
@@ -10,7 +11,30 @@ export class ContactData extends Component {
       street: '',
       zip: '',
     },
+    loading: false,
   };
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    
+    this.setState({loading: true})
+    const order = {
+        ingredients: this.props.ingredients,
+        price: this.props.totalPrice,  // should calculate on server
+        customer: {
+            name: 'Ben',
+            address: '123 Main St.',
+            zip: '11111',
+            email: 'test@test.com',
+        }
+    }
+    axios.post('/orders.json', order)
+         .then(response => this.setState({loading: false}))
+         .catch(error => {
+             this.setState({loading: false});
+             console.log('Error ' + error);
+         });
+  }
 
   render() {
     return (
@@ -18,10 +42,10 @@ export class ContactData extends Component {
         <h4>Enter contact data</h4>  
         <Form>
           <Form.Input label='Name' type='text' placeholder='Your Name' />
-          <Form.Input label='Email' type='email' />
+          <Form.Input label='Email' type='email' value='a@a.com'/>
           <Form.Input label='Street' type='text' placeholder='Street' />
           <Form.Input label='Zip' type='number' />
-          <Form.Button content='Order' />
+          <Form.Button onClick={this.orderHandler} content='Order' />
         </Form>
       </Card>
     )
