bc6fef5e2122228aed15d22a767762ad19742217
Ben Wiedermann <benw@cs.hmc.edu>
2018-04-17 01:04:54 -0700

    using environment variables to conditionally show REDUX dev tools

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import authReducer from './store/reducers/auth';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development'
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  : compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer, 
